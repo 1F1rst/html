9f00d00c11e4c320aa4d361061d694ee39160d27
1F1rst <162057812+1F1rst@users.noreply.github.com>
2024-03-03 23:47:50 -0300

    Initial commit

---
+++ python/main.py
@@ -0,0 +1,26 @@
+nome = input("Digite seu nome:")
+idade = int(input("Digite sua idade:"))
+peso = float(input("Digite seu peso:"))
+
+if nome = "raphael":
+    print("Permissão Liberada")
+else:
+    print("Permissão Negada")
+
+if idade = 18:
+    print("Permissão Liberada")
+else:
+    print("Permissão Negada")
+
+if peso = 56.8:
+    print("Permissão Liberada")
+else:
+    print("Permissão Negada")
+
+
+
+
+
+
+
+
